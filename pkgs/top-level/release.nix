2a83d0049349038e9283d8c07065f9ab487b9219
Christopher Burr <christopher.burr@cern.ch>
2017-10-09 09:29:39 +0100

    Possibly disable darwin builds

--- pkgs/top-level/release.nix
+++ pkgs/top-level/release.nix
@@ -11,8 +11,8 @@
 { nixpkgs ? { outPath = (import ../../lib).cleanSource ../..; revCount = 1234; shortRev = "abcdef"; }
 , officialRelease ? false
   # The platforms for which we build Nixpkgs.
-, supportedSystems ? [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" ]
-, limitedSupportedSystems ? [ "i686-linux" ]
+, supportedSystems ? [ "x86_64-linux" ]
+, limitedSupportedSystems ? [ ]
   # Strip most of attributes when evaluating to spare memory usage
 ,  scrubJobs ? true
   # Attributes passed to nixpkgs. Don't build packages marked as unfree.
@@ -33,21 +33,21 @@ let
       manual = import ../../doc;
       lib-tests = import ../../lib/tests/release.nix { inherit pkgs; };
 
-      darwin-tested = pkgs.releaseTools.aggregate
-        { name = "nixpkgs-darwin-${jobs.tarball.version}";
-          meta.description = "Release-critical builds for the Nixpkgs darwin channel";
-          constituents =
-            [ jobs.tarball
-              jobs.stdenv.x86_64-darwin
-              jobs.ghc.x86_64-darwin
-              jobs.cabal2nix.x86_64-darwin
-              jobs.ruby.x86_64-darwin
-              jobs.python.x86_64-darwin
-              jobs.rustc.x86_64-darwin
-              jobs.go.x86_64-darwin
-              jobs.tests.macOSSierraShared.x86_64-darwin
-            ];
-        };
+      # darwin-tested = pkgs.releaseTools.aggregate
+      #   { name = "nixpkgs-darwin-${jobs.tarball.version}";
+      #     meta.description = "Release-critical builds for the Nixpkgs darwin channel";
+      #     constituents =
+      #       [ jobs.tarball
+      #         jobs.stdenv.x86_64-darwin
+      #         jobs.ghc.x86_64-darwin
+      #         jobs.cabal2nix.x86_64-darwin
+      #         jobs.ruby.x86_64-darwin
+      #         jobs.python.x86_64-darwin
+      #         jobs.rustc.x86_64-darwin
+      #         jobs.go.x86_64-darwin
+      #         jobs.tests.macOSSierraShared.x86_64-darwin
+      #       ];
+      #   };
 
       unstable = pkgs.releaseTools.aggregate
         { name = "nixpkgs-${jobs.tarball.version}";
@@ -58,45 +58,45 @@ let
               jobs.manual
               jobs.lib-tests
               jobs.stdenv.x86_64-linux
-              jobs.stdenv.x86_64-darwin
+              # jobs.stdenv.x86_64-darwin
               jobs.linux.x86_64-linux
               jobs.python.x86_64-linux
-              jobs.python.x86_64-darwin
+              # jobs.python.x86_64-darwin
               jobs.python3.x86_64-linux
-              jobs.python3.x86_64-darwin
+              # jobs.python3.x86_64-darwin
               # Many developers use nix-repl
               jobs.nix-repl.x86_64-linux
-              jobs.nix-repl.x86_64-darwin
+              # jobs.nix-repl.x86_64-darwin
               # Needed by travis-ci to test PRs
               jobs.nox.x86_64-linux
-              jobs.nox.x86_64-darwin
+              # jobs.nox.x86_64-darwin
               # Ensure that X11/GTK+ are in order.
               jobs.thunderbird.x86_64-linux
               # Ensure that basic stuff works on darwin
-              jobs.git.x86_64-darwin
-              jobs.mysql.x86_64-darwin
-              jobs.vim.x86_64-darwin
+              # jobs.git.x86_64-darwin
+              # jobs.mysql.x86_64-darwin
+              # jobs.vim.x86_64-darwin
             ] ++ lib.collect lib.isDerivation jobs.stdenvBootstrapTools;
         };
 
-      stdenvBootstrapTools = with lib;
-        genAttrs systemsWithAnySupport
-          (system: {
-            inherit (import ../stdenv/linux/make-bootstrap-tools.nix { inherit system; })
-              dist test;
-          })
-        # darwin is special in this
-        // optionalAttrs (builtins.elem "x86_64-darwin" systemsWithAnySupport) {
-          x86_64-darwin =
-            let
-              bootstrap = import ../stdenv/darwin/make-bootstrap-tools.nix { system = "x86_64-darwin"; };
-            in {
-              # Lightweight distribution and test
-              inherit (bootstrap) dist test;
-              # Test a full stdenv bootstrap from the bootstrap tools definition
-              inherit (bootstrap.test-pkgs) stdenv;
-            };
-          };
+      # stdenvBootstrapTools = with lib;
+      #   genAttrs systemsWithAnySupport
+      #     (system: {
+      #       inherit (import ../stdenv/linux/make-bootstrap-tools.nix { inherit system; })
+      #         dist test;
+      #     })
+      #   # darwin is special in this
+      #   // optionalAttrs (builtins.elem "x86_64-darwin" systemsWithAnySupport) {
+      #     x86_64-darwin =
+      #       let
+      #         bootstrap = import ../stdenv/darwin/make-bootstrap-tools.nix { system = "x86_64-darwin"; };
+      #       in {
+      #         # Lightweight distribution and test
+      #         inherit (bootstrap) dist test;
+      #         # Test a full stdenv bootstrap from the bootstrap tools definition
+      #         inherit (bootstrap.test-pkgs) stdenv;
+      #       };
+      #     };
 
     } // (mapTestOn ((packagePlatforms pkgs) // rec {
       haskell.compiler = packagePlatforms pkgs.haskell.compiler;
@@ -109,11 +109,11 @@ let
       ocamlPackages = { };
       perlPackages = { };
 
-      darwin = packagePlatforms pkgs.darwin // {
-        cf-private = {};
-        osx_private_sdk = {};
-        xcode = {};
-      };
+      # darwin = packagePlatforms pkgs.darwin // {
+      #   cf-private = {};
+      #   osx_private_sdk = {};
+      #   xcode = {};
+      # };
     } ));
 
 in jobs

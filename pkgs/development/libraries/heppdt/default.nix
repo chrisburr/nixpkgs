d03320131403810da17424242931a7f0e76eb2ed
Christopher Burr <christopher.burr@cern.ch>
2017-10-09 01:48:26 +0100

    Downgrade HepPDT to v2

--- pkgs/development/libraries/heppdt/default.nix
+++ pkgs/development/libraries/heppdt/default.nix
@@ -1,15 +1,17 @@
 { stdenv, fetchurl, automake, autoconf, ghostscript }:
 
 let
-  version = "3.04.01";
+  version = "2.06.01";
   name = "heppdt-${version}";
 in stdenv.mkDerivation {
   inherit name;
   inherit version;
 
   src = fetchurl {
+    # url = "http://lcgapp.cern.ch/project/simu/HepPDT/download/HepPDT-3.04.01.tar.gz";
+    # sha256 = "1xn0ccpp4pgxbxr7pqydy31mpd8wifiz3lz0d7nksp99j7mkj71c";
     url = "http://lcgapp.cern.ch/project/simu/HepPDT/download/HepPDT-${version}.tar.gz";
-    sha256 = "1xn0ccpp4pgxbxr7pqydy31mpd8wifiz3lz0d7nksp99j7mkj71c";
+    sha256 = "0bicg9380j61p8adckvd7f7nz8wmdr7z8w2d7gx06rk2vpzvd88j";
   };
 
   preConfigure = ''
